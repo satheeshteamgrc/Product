99d854f1ffd47f0e56c9dff6e85f619fd3ec7790
satheeshteamgrc <satheesh147@gmail.com>
2017-01-18 15:28:12 +0530

    just change

--- ProductApplication/main form.vb
+++ ProductApplication/main form.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub CLick_btn_Click(sender As Object, e As EventArgs) Handles CLick_btn.Click
-        MsgBox("success meet dfsdfsdf")
+        MsgBox("success meet")
     End Sub
 End Class
